687294978d7b84186a626c7ca75275cad1665f57
chris hughes <christopher.hughes@georgebrown.ca>
2019-12-10 10:04:36 -0500

    back end final

--- routes/business.route.js
+++ routes/business.route.js
@@ -5,7 +5,7 @@ const app = express();
 const businessRoutes = express.Router();
 
 // Require Business model in our routes module
-let Business = require('https://ac-backend-prod.herokuapp.com/api/models/Business');
+let Business = require('https://ac-backend-prod.herokuapp.com/Business');
 
 
 // Defined store route
